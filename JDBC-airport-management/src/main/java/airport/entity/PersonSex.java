735fc4f228a6a76f7bd1447eb85ceb9d04057b26
Dmitrii Korneev <kdmitry.de@gmail.com>
2025-09-12 13:37:34 +0200

    Added entity classes for dictionary tables

---
+++ JDBC-airport-management/src/main/java/airport/entity/PersonSex.java
@@ -0,0 +1,36 @@
+package airport.entity;
+
+public class PersonSex {
+
+    private int id;
+    private String name;
+
+    public PersonSex(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonSex{" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                '}';
+    }
+}
---
+++ JDBC-airport-management/src/main/java/airport/entity/Terminal.java
@@ -0,0 +1,36 @@
+package airport.entity;
+
+public class Terminal {
+
+    private int id;
+    private String name;
+
+    public Terminal(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Terminal{" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                '}';
+    }
+}
